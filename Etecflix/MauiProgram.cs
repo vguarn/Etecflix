1c109d76f20de9e641ecb6506e7bb88fb61dbb07
Usuario <Usuario@CPD03-M15>
2024-05-06 14:34:45 -0300

    alterações no projeto.

--- Etecflix/MauiProgram.cs
+++ Etecflix/MauiProgram.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Etecflix;
+using Microsoft.Extensions.Logging;
 
-namespace Etecflix
+namespace MauiAppCinema
 {
     public static class MauiProgram
     {
